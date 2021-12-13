2d89eb46843e1934df7dc6fd6559093582725849
sabrinaweng <mweng1@mail.sfsu.edu>
2021-12-13 14:08:00 -0800

    update header button text "register"

--- Team12_CSC642/routes/index.js
+++ Team12_CSC642/routes/index.js
@@ -7,13 +7,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET page. */
+
 router.get('/', function(req, res, next) {
   res.render('HomePage');
 });
 
-router.get('/HomeResultsPage', function(req, res, next) {
-  res.render('HomeResultsPage');
+router.get('/ResultsPage', function(req, res, next) {
+  res.render('ResultsPage');
 });
 
 router.get('/Login', function(req, res, next) {
